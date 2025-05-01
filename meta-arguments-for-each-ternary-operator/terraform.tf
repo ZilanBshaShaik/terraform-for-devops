6577a9a25c6134e234f004c4a3ddacec4dfe43b5
Shaik Zilan Basha <br.bashatechz@gmail.com>
2025-05-01 17:21:17 +0530

    added environment details

--- meta-arguments-for-each-ternary-operator/terraform.tf
+++ meta-arguments-for-each-ternary-operator/terraform.tf
@@ -6,10 +6,4 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "tws-junoon-state-bucket-remote"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "tws-junoon-state-table"
-  }
 }
